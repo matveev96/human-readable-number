c282e49b92566c6553eaf3420d02a70f36cc46a6
matveev96 <sasha.matveev1996@icloud.com>
2024-08-05 20:23:43 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function toReadable (number) {
-  
+    const a = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    const b = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const c = ['', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    const d = 'hundred';
+    const e = 'zero';
+    const f = 'ten';
+
+    let string = number.toString();
+    let final_num = '';
+
+    if (number == 0) return e;
+    else if (string.length == 1) {
+        return a[number - 1];
+    } else if (string.length == 2) {
+        final_num = 
+        (number == 10) ? f : (parseInt(string[0]) == 1) ?  b[parseInt(string[1]) - 1] : 
+        (parseInt(string[1]) == 0) ? c[parseInt(string[0]) - 1] : c[parseInt(string[0]) - 1] + ' ' + a[parseInt(string[1]) -1];
+    } else if (string.length == 3) {
+        final_num = 
+        (parseInt(string[1]) == 0 && parseInt(string[2]) == 0) ? a[parseInt(string[0] - 1)] + ' ' + d : 
+        (parseInt(string[1]) == 1) ? a[parseInt(string[0] - 1)] + ' ' + d + ' ' + (b[parseInt(string[2] - 1)] || f) : 
+        (parseInt(string[1]) == 0) ? a[parseInt(string[0] - 1)] + ' ' + d + ' ' + a[parseInt(string[2] - 1)] : 
+        (parseInt(string[2]) == 0) ? a[parseInt(string[0] - 1)] + ' ' + d + ' ' + c[parseInt(string[1] - 1)] : 
+        a[parseInt(string[0] - 1)] + ' ' + d + ' ' + c[parseInt(string[1] - 1)] + ' ' + a[parseInt(string[2]) - 1];
+    };
+    return final_num;
 }
